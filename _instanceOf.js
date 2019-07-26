e3611063dc2a43dc796511d22d5facae88d66ffe
chiic <chiic1101@163.com>
2019-07-26 11:14:27 +0800

    modify instanceof

--- _instanceOf.js
+++ _instanceOf.js
@@ -2,14 +2,14 @@ function _instanceOf(aSon, pT) {
     if(aSon === undefined || aSon === null) {
         throw new Error('Cannot set undefined or null');
     } else {
-        var proto = Object.getPrototypeOf(aSon);
+        var proto = aSon.__proto__;
         while(proto !== null) {
             if(proto === pT.prototype) return true;
-            proto = Object.getPrototypeOf(proto);
+            proto = proto.__proto__;
         }
         return false;
     }
 }
 
-var p = _instanceOf(undefined, Object);
+var p = _instanceOf({}, Function);
 console.log(p)
